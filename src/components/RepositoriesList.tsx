79e6e4788d51cd5d5ade45b1e3e82cf86c84c35e
sidhant singh <singhsidmail23@gmail.com>
2023-03-26 22:12:58 +0530

    npm registry search app

---
+++ src/components/RepositoriesList.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { useActions } from "../hooks/useaction";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+
+const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState("");
+  const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  );
+
+  const onsubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    searchRepositories(term);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onsubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
+        <button>Search</button>
+      </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data}
+    </div>
+  );
+};
+
+export default RepositoriesList;
